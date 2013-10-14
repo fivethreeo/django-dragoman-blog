ca7b7c15a9d68d551b8dac67e1b2eca43e8b594f
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2013-10-14 11:33:22 +0200

    date based views without language filtering working

--- hvad_blog/urls.py
+++ hvad_blog/urls.py
@@ -22,13 +22,13 @@ blog_detail = DateDetailView.as_view(
 urlpatterns = patterns('',
     url(r'^$', ArchiveIndexView.as_view(**blog_info_dict), name='hvad_blog_archive_index'),
     
-    url(r'^(?P<year>\d{4})/$', YearArchiveView.as_view(), name='hvad_blog_archive_year'),
+    url(r'^(?P<year>\d{4})/$', YearArchiveView.as_view(**blog_info_dict), name='hvad_blog_archive_year'),
     
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
-        MonthArchiveView.as_view(), name='hvad_blog_archive_month'),
+        MonthArchiveView.as_view(month_format='%m', **blog_info_dict), name='hvad_blog_archive_month'),
     
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        DayArchiveView.as_view(), name='hvad_blog_archive_day'),
+        DayArchiveView.as_view(month_format='%m', **blog_info_dict), name='hvad_blog_archive_day'),
     
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
         blog_detail, name='hvad_blog_detail')
