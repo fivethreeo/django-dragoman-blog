05ad5917e77eb0783967486ce50673dd356122dd
Vladimir Bolshakov <vovanbo@gmail.com>
2013-11-04 17:31:00 +0400

    Added list by tag.

--- dragoman_blog/urls.py
+++ dragoman_blog/urls.py
@@ -5,32 +5,38 @@ from dragoman_blog.models import Entry, EntryTranslation
 
 from django.utils.translation import get_language
 from django.views.generic.dates import DateDetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView
+from dragoman_blog.views import ListByTagView
+
 
 class EntryDateDetail(DateDetailView):
-    
+
     def get_queryset(self):
         return super(EntryDateDetail, self).get_queryset().filter(language_code=get_language())
-        
+
+
 class EntryArchiveIndex(ArchiveIndexView):
-    
+
     def get_queryset(self):
         return super(EntryArchiveIndex, self).get_queryset().filter(language_code=get_language())
-        
+
+
 class EntryYearArchive(YearArchiveView):
-    
+
     def get_queryset(self):
         return super(EntryYearArchive, self).get_queryset().filter(language_code=get_language())
-        
+
+
 class EntryMonthArchive(MonthArchiveView):
-    
+
     def get_queryset(self):
         return super(EntryMonthArchive, self).get_queryset().filter(language_code=get_language())
-        
+
+
 class EntryDayArchive(DayArchiveView):
-    
+
     def get_queryset(self):
         return super(EntryDayArchive, self).get_queryset().filter(language_code=get_language())
-        
+
 
 blog_info_dict = {
     'queryset': EntryTranslation.objects.all(),
@@ -47,16 +53,23 @@ blog_detail = EntryDateDetail.as_view(
 )
 
 urlpatterns = patterns('',
-    url(r'^$', EntryArchiveIndex.as_view(**blog_info_dict), name='dragoman_blog_archive_index'),
-    
-    url(r'^(?P<year>\d{4})/$', EntryYearArchive.as_view(**blog_info_dict), name='dragoman_blog_archive_year'),
-    
+    url(r'^$', EntryArchiveIndex.as_view(**blog_info_dict),
+       name='dragoman_blog_archive_index'),
+
+    url(r'^(?P<year>\d{4})/$', EntryYearArchive.as_view(**blog_info_dict),
+        name='dragoman_blog_archive_year'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
-        EntryMonthArchive.as_view(month_format='%m', **blog_info_dict), name='dragoman_blog_archive_month'),
-    
+       EntryMonthArchive.as_view(month_format='%m', **blog_info_dict),
+       name='dragoman_blog_archive_month'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        EntryDayArchive.as_view(month_format='%m', **blog_info_dict), name='dragoman_blog_archive_day'),
-    
+       EntryDayArchive.as_view(month_format='%m', **blog_info_dict),
+       name='dragoman_blog_archive_day'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-        blog_detail, name='dragoman_blog_detail')
+       blog_detail, name='dragoman_blog_detail'),
+
+    url(r"^tag/(?P<tag>.*)/$", ListByTagView.as_view(),
+       name="dragoman_blog_list_by_tag"),
 )
---
+++ dragoman_blog/views.py
@@ -0,0 +1,25 @@
+from dragoman_blog.models import EntryTranslation
+from django.views.generic.list import ListView
+
+
+class ListByTagView(ListView):
+
+    """ View for listing posts by tags"""
+    template_name = "dragoman_blog/entrytranslation_list_by_tag.html"
+    model = EntryTranslation
+
+    def get_queryset(self):
+        try:
+            tag = self.kwargs['tag']
+        except:
+            tag = ''
+        if (tag != ''):
+            object_list = self.model.objects.filter(tags__name=tag)
+        else:
+            object_list = self.model.objects.none()
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super(ListByTagView, self).get_context_data(**kwargs)
+        context['tag'] = self.kwargs['tag']
+        return context
